f7a41f9a47a68df233e2b3a3573c20555e36b12c
Florian Muellner <florian.muellner@gmail.com>
2009-01-12 14:09:04 +0000

    Restructure repository
    Finish migration to proper repository structure

---
+++ gami/gami.h
@@ -0,0 +1,11 @@
+#ifndef __GAMI_H__
+#define __GAMI_H__
+
+#define __GAMI_H_INSIDE__
+
+#include <gami/gami-main.h>
+#include <gami/gami-manager.h>
+#include <gami/gami-response.h>
+
+#undef __GAMI_H_INSIDE__
+#endif
